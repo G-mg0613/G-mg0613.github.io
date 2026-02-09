f6e4478efa45d8b25bd4f5be99d655f66b516f88
G-mg0613 <afsfdd@aol.com>
2026-02-09 23:27:04 +0800

    Site updated: 2026-02-09 23:27:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770624440202";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770650811295";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
